0e5a240cd675c645dbe968be5c103c5894848be1
rmarita <rmarita@palcnetworks.com>
2021-07-26 15:18:23 +0530

    added batchrun.bat file

---
+++ BatchRun.bat
@@ -0,0 +1,9 @@
+set projectLocation=D:\GitHub Project\testProject
+
+cd D:\GitHub Project\testProject
+
+set classpath=D:\GitHub Project\testProject\bin;D:\GitHub Project\testProject\jar\*
+
+java org.testng.TestNG D:\GitHub Project\testProject\testng.xml
+
+pause
